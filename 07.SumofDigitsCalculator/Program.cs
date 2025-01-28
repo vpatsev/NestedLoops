979f6e9cdb7bcbf49c75fcf5f9068144f991ee84
Ventsislav Patsev <164578745+vpatsev@users.noreply.github.com>
2025-01-28 22:50:02 +0200

    Add files via upload

---
+++ 07.SumofDigitsCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿string input = Console.ReadLine();    
+
+while (input != "End")
+{
+  int num = int.Parse(input);
+    int sum = 0;
+    
+    while (num > 0) {
+        int lastNum = num % 10;
+        sum += lastNum;
+        num /= 10;
+    }
+    Console.WriteLine($"Sum of digits = {sum}");
+    input = Console.ReadLine();
+
+    }
+Console.WriteLine("Goodbye");
---
+++ 08.PrimeNums/Program.cs
@@ -0,0 +1,18 @@
+﻿int startNum = int.Parse(Console.ReadLine());
+int endNum = int.Parse(Console.ReadLine());
+
+for (int i = startNum; i <= endNum; i++)
+{
+    int primeCounter = 0;
+    for (int j = 1; j <= i; j++)
+    {
+        if (i % j == 0) { 
+        primeCounter++;
+    }
+    }
+    if (primeCounter == 2)
+    {
+        Console.Write($"{i} ");
+    }
+}
+
